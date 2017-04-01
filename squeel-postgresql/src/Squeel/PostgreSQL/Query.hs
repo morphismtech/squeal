502816a7b4a166553b7e64e2ca9ec4ac64dc0e2d
Eitan Chatav <eitan.chatav@gmail.com>
2017-04-01 12:23:21 -0400

    query work

--- squeel-postgresql/src/Squeel/PostgreSQL/Query.hs
+++ squeel-postgresql/src/Squeel/PostgreSQL/Query.hs
@@ -1,11 +1,194 @@
 {-# LANGUAGE
     PolyKinds
+  , DataKinds
+  , FlexibleContexts
+  , FlexibleInstances
+  , GADTs
+  , KindSignatures
+  , LambdaCase
+  , OverloadedStrings
+  , RankNTypes
+  , ScopedTypeVariables
+  , TypeApplications
+  , TypeInType
+  , TypeOperators
 #-}
+
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
 module Squeel.PostgreSQL.Query where
 
 import Data.ByteString (ByteString)
+import Data.Monoid
+import Data.Proxy
+import Data.String
+-- import Data.Vinyl
+import GHC.TypeLits
 
-newtype Query db0 db1 ps xs = Query { unQuery :: ByteString }
+import qualified Data.ByteString.Char8 as Char8
+
+import Squeel.PostgreSQL.Type
+
+data Expression (ps :: [PGType]) (cols :: [Column]) (x :: PGType) =
+  Expression { unExpression :: ByteString } deriving Show
+
+param1 :: Expression (p1:ps) cols p1
+param1 = Expression "$1"
+
+param2 :: Expression (p1:p2:ps) cols p2
+param2 = Expression "$2"
+
+param3 :: Expression (p1:p2:p3:ps) cols p3
+param3 = Expression "$3"
+
+param4 :: Expression (p1:p2:p3:p4:ps) cols p4
+param4 = Expression "$4"
+
+param5 :: Expression (p1:p2:p3:p4:p5:ps) cols p5
+param5 = Expression "$5"
+
+whenThen :: (Expression ps cols x,Expression ps cols y) -> ByteString
+whenThen (Expression when_,Expression then_) = mconcat
+  ["  WHEN ",when_," THEN ",then_,"\n"]
+
+case_
+  :: [(Expression ps cols ('PGType "bool"),Expression ps cols x)]
+  -> Expression ps cols x
+  -> Expression ps cols x
+case_ whens (Expression else_) = Expression $ mconcat
+  [ "CASE\n"
+  , mconcat (map whenThen whens)
+  , "  ELSE ",else_
+  , "\nEND"
+  ]
+
+caseSwitch
+  :: Expression ps cols x
+  -> [(Expression ps cols x,Expression ps cols y)]
+  -> Expression ps cols y
+  -> Expression ps cols y
+caseSwitch (Expression val) whens (Expression else_) = Expression $ mconcat
+  [ "CASE ",val,"\n"
+  , mconcat (map whenThen whens)
+  , "  ELSE ",else_
+  ,"\nEND"
+  ]
+
+column
+  :: forall colName colType params columns
+  .  Alias (Expression params columns) (colName ':= colType)
+  -> Expression params columns colType
+column = \case
+  As x -> x
+  Elem -> Expression $ Char8.pack (symbolVal (Proxy @colName))
+
+true :: Expression ps cols ('PGType "bool")
+true = Expression "true"
+
+false :: Expression ps cols ('PGType "bool")
+false = Expression "false"
+
+(.==)
+  :: Expression ps cols x
+  -> Expression ps cols x
+  -> Expression ps cols ('PGType "bool")
+(.==) (Expression x1) (Expression x2) = Expression $ x1 <> "=" <> x2
+
+(./=)
+  :: Expression ps cols x
+  -> Expression ps cols x
+  -> Expression ps cols ('PGType "bool")
+(./=) (Expression x1) (Expression x2) = Expression $ x1 <> "<>" <> x2
+
+(.>)
+  :: Expression ps cols x
+  -> Expression ps cols x
+  -> Expression ps cols ('PGType "bool")
+(.>) (Expression x1) (Expression x2) = Expression $ x1 <> ">" <> x2
+
+(.<)
+  :: Expression ps cols x
+  -> Expression ps cols x
+  -> Expression ps cols ('PGType "bool")
+(.<) (Expression x1) (Expression x2) = Expression $ x1 <> "<" <> x2
+
+(.>=)
+  :: Expression ps cols x
+  -> Expression ps cols x
+  -> Expression ps cols ('PGType "bool")
+(.>=) (Expression x1) (Expression x2) = Expression $ x1 <> ">=" <> x2
+
+(.<=)
+  :: Expression ps cols x
+  -> Expression ps cols x
+  -> Expression ps cols ('PGType "bool")
+(.<=) (Expression x1) (Expression x2) = Expression $ x1 <> "<=" <> x2
+
+instance IsString (Expression ps cols ('PGType "text")) where
+  fromString str = Expression $ "\'" <> fromString str <> "\'"
+
+instance Num (Expression ps cols ('PGType "int4")) where
+  fromInteger n = Expression . fromString $ show n
+  Expression x + Expression y = Expression $ x <> "+" <> y
+  Expression x * Expression y = Expression $ x <> "*" <> y
+  Expression x - Expression y = Expression $ x <> "+" <> y
+  abs (Expression x) = Expression $ "@" <> x
+  signum (Expression x) = Expression $ "sign(" <> x <> ")"
+
+data Relation
+  (ps :: [PGType])
+  (xs :: [Column])
+  (db :: [Table])
+  (ys :: [Column])
+    = Relation { unRelation :: ByteString }
+
+-- star :: Relation db params xs xs
+-- star = Relation "SELECT * FROM "
+
+-- table
+--   :: forall tableName tableCols db params.
+--     ( KnownSymbol tableName
+--     , (tableName ':= tableCols) `Elem` Unnamed db ~ 'True
+--     ) => Proxy tableName -> Relation db params '[] tableCols
+-- table (_ :: Proxy tableName) = Relation $
+--   "SELECT * FROM " <> Char8.pack (symbolVal (Proxy @tableName))
+
+starFrom
+  :: forall tableName tableCols db params columns
+  .  Alias (Relation params columns db) (tableName ':= tableCols)
+  -> Relation params columns db tableCols
+starFrom = \case
+  As x -> x
+  Elem -> Relation $
+    "SELECT * FROM " <> Char8.pack (symbolVal (Proxy @tableName))
+
+-- from
+--   :: Relation db params ys zs
+--   -> Relation db params xs ys
+--   -> Relation db params xs zs
+-- from (Relation rel1) (Relation rel2) = Relation $ rel1 <> rel2
+
+-- project :: Rec (Alias (Expression ps xs)) ys -> Relation db params xs ys
+-- project exprs = Relation $
+--   "SELECT " <> Char8.intercalate "," (exprStrings exprs) <> " FROM "
+--   where
+--     exprStrings :: Rec (Alias (Expression ps xs)) ys -> [ByteString]
+--     exprStrings = \case
+--       RNil -> []
+--       (As (Expression expr) :: Alias (Expression _ _) (exprName ':= _))
+--         :& exprs ->
+--           (expr <> " As " <> Char8.pack (symbolVal (Proxy @exprName)))
+--             : expressionStrings exprs
+
+newtype Query
+  (db0 :: [Table])
+  (db1 :: [Table])
+  (ps :: [PGType])
+  (xs :: [Column])
+    = Query { unQuery :: ByteString }
+
 newtype PreparedQuery db0 db1 ps xs
   = PreparedQuery { unPreparedQuery :: ByteString }
+
+select :: Relation ps ('[] :: [Column]) db xs -> Query db db ps xs
+select (Relation x) = Query x
--- squeel-postgresql/src/Squeel/PostgreSQL/Type.hs
+++ squeel-postgresql/src/Squeel/PostgreSQL/Type.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE
     DataKinds
+  , GADTs
   , PolyKinds
   , ScopedTypeVariables
   , TypeApplications
@@ -14,43 +15,26 @@ import qualified Database.PostgreSQL.LibPQ as LibPQ
 import GHC.TypeLits
 
 newtype PGType = PGType Symbol
-data Column = Column Symbol PGType
-data Table = Table Symbol [Column]
-data Database = Database Symbol [Table]
-data Schema = Schema Symbol [Database]
+data Named x = (:=) Symbol x
+type family Unnamed named where Unnamed (name ':= x) = x
+type family Name x where Name (name ':= x) = name
+type Column = Named PGType
+type Table = Named [Column]
+type Database = Named [Table]
+type Schema = Named [Database]
+data Alias (obj :: k -> *) (named :: Named k) where
+  As :: KnownSymbol name => obj x -> Alias obj (name ':= x)
+  Elem
+    :: (KnownSymbol name, (name ':= x) `Elem` list ~ 'True)
+    => Alias (obj list) (name ':= x)
 
-type family HasColumn table column where
-  HasColumn ('Table table '[]) column = 'False
-  HasColumn ('Table table (column ': columns)) column = 'True
-  HasColumn ('Table table (column ': columns)) column'
-    = HasColumn ('Table table columns) column'
+type family Elem x xs where
+  x `Elem` '[] = 'False
+  x `Elem` (x ': xs) = 'True
+  x `Elem` (x' ': xs) = x `Elem` xs
 
-type family HasTable database table where
-  HasTable ('Database database '[]) table = 'False
-  HasTable ('Database database (table ': tables)) table = 'True
-  HasTable ('Database database (table ': tables)) table'
-    = HasTable ('Database database tables) table'
-
-type family HasDatabase schema database where
-  HasDatabase ('Schema schema '[]) database = 'False
-  HasDatabase ('Schema schema (database ': databases)) database = 'True
-  HasDatabase ('Schema schema (database ': databases)) database'
-    = HasDatabase ('Schema schema databases) database'
-
-type family TableColumns table where
-  TableColumns ('Table table columns) = columns
-
-type family DatabaseTables database where
-  DatabaseTables ('Database database tables) = tables
-
-type family SchemaDatabases schema where
-  SchemaDatabases ('Schema schema databases) = databases
-
-class ToOid pg where
-  toOid :: Proxy pg -> LibPQ.Oid
-
-class ToOids pgs where
-  toOids :: Proxy pgs -> [LibPQ.Oid]
+class ToOid pg where toOid :: Proxy pg -> LibPQ.Oid
+class ToOids pgs where toOids :: Proxy pgs -> [LibPQ.Oid]
 instance ToOids '[] where toOids _ = []
 instance (ToOid pg, ToOids pgs) => ToOids (pg ': pgs) where
   toOids (_ :: Proxy (pg ': pgs)) = toOid (Proxy @pg) : toOids (Proxy @pgs)
