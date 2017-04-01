3771b5f02cf1c18d3810c60d4ceee4ed184b3724
Eitan Chatav <eitan.chatav@gmail.com>
2017-04-01 12:23:55 -0400

    mtl style PQ interface

--- squeel-postgresql/src/Squeel/PostgreSQL/PQ.hs
+++ squeel-postgresql/src/Squeel/PostgreSQL/PQ.hs
@@ -1,26 +1,22 @@
 {-# LANGUAGE
-    DataKinds
-  , DeriveFunctor
+    DeriveFunctor
   , FlexibleContexts
   , FlexibleInstances
   , MultiParamTypeClasses
-  , PolyKinds
   , RankNTypes
-  , RecordWildCards
   , ScopedTypeVariables
   , TypeApplications
   , TypeFamilies
+  , TypeInType
   , TypeOperators
   , UndecidableInstances
 #-}
 
 module Squeel.PostgreSQL.PQ where
 
-import Control.Arrow
 import Control.Exception.Lifted
 import Control.Monad.Base
-import Control.Monad.Indexed
-import Control.Monad.Indexed.Trans
+import Control.Monad.Trans
 import Control.Monad.Trans.Control
 import Data.ByteString (ByteString)
 import Data.Proxy
@@ -36,57 +32,118 @@ import Squeel.PostgreSQL.Type
 
 newtype Connection db = Connection { unConnection :: LibPQ.Connection }
 
-newtype PQ m db0 db1 x = PQ
+newtype PQ (db0 :: [Table]) (db1 :: [Table]) m x = PQ
   { runPQ :: Connection db0 -> m (x, Connection db1) }
   deriving Functor
 
-evalPQ :: Functor m => PQ m db0 db1 x -> Connection db0 -> m x
+evalPQ :: Functor m => PQ db0 db1 m x -> Connection db0 -> m x
 evalPQ (PQ pq) = fmap fst . pq
 
-execPQ :: Functor m => PQ m db0 db1 x -> Connection db0 -> m (Connection db1)
+execPQ :: Functor m => PQ db0 db1 m x -> Connection db0 -> m (Connection db1)
 execPQ (PQ pq) = fmap snd . pq
 
-instance Functor m => IxFunctor (PQ m) where
-  imap f (PQ g) = PQ $ fmap (first f) . g
+class MonadPQ pq where
 
-instance Applicative m => IxPointed (PQ m) where
-  ireturn x = PQ $ \ conn -> pure (x, conn)
+  pqAp
+    :: Monad m
+    => pq db0 db1 m (x -> y)
+    -> pq db1 db2 m x
+    -> pq db0 db2 m y
 
-instance Monad m => IxApplicative (PQ m) where
-  iap (PQ f) (PQ x) = PQ $ \ conn -> do
+  pqBind
+    :: Monad m
+    => (x -> pq db1 db2 m y)
+    -> pq db0 db1 m x
+    -> pq db0 db2 m y
+
+  pqExec
+    :: MonadBase IO io
+    => Query db0 db1 '[] xs
+    -> pq db0 db1 io (Maybe (Result xs))
+
+  pqExecParams
+    :: (MonadBase IO io, ToValues xs ps, ToOids ps)
+    => Query db0 db1 ps ys
+    -> Rec Identity xs
+    -> pq db0 db1 io (Maybe (Result ys))
+
+  pqPrepare
+    :: (MonadBase IO io, ToOids ps)
+    => ByteString
+    -> Query db0 db1 ps xs
+    -> pq db0 db1 io (Maybe (Result []), PreparedQuery db0 db1 ps xs)
+
+  pqExecPrepared
+    :: (MonadBase IO io, ToValues xs ps)
+    => PreparedQuery db0 db1 ps ys
+    -> Rec Identity xs
+    -> pq db0 db1 io (Maybe (Result ys))
+
+instance MonadPQ PQ where
+
+  pqAp (PQ f) (PQ x) = PQ $ \ conn -> do
     (f', conn') <- f conn
     (x', conn'') <- x conn'
     return (f' x', conn'')
 
-instance Monad m => Applicative (PQ m db db) where
-  pure = ireturn
-  (<*>) = iap
-
-instance Monad m => IxMonad (PQ m) where
-  ibind f (PQ x) = PQ $ \ conn -> do
+  pqBind f (PQ x) = PQ $ \ conn -> do
     (x', conn') <- x conn
     runPQ (f x') conn'
 
-instance Monad m => Monad (PQ m db db) where
-  return = ireturn
-  (>>=) = flip ibind
+  pqExec (Query q) = PQ $ \ (Connection conn) -> do
+    result <- liftBase $ LibPQ.exec conn q
+    return (Result <$> result, Connection conn)
 
-instance IxMonadTrans PQ where
-  ilift m = PQ $ \ conn -> do
+  pqExecParams (Query q :: Query db0 db1 ps ys) params =
+    PQ $ \ (Connection conn) -> do
+      let
+        params' =
+          [ Just (oid, param, LibPQ.Binary)
+          | (oid, param) <-
+              zip (toOids (Proxy @ps)) (toValues (Proxy @ps) params)
+          ]
+      result <- liftBase $ LibPQ.execParams conn q params' LibPQ.Binary
+      return (Result <$> result, Connection conn)
+
+  pqPrepare statementName (Query q :: Query db0 db1 ps ys) =
+    PQ $ \ (Connection conn) -> do
+      result <- liftBase $
+        LibPQ.prepare conn statementName q (Just (toOids (Proxy @ps)))
+      return ((Result <$> result,PreparedQuery statementName), Connection conn)
+
+  pqExecPrepared (PreparedQuery q :: PreparedQuery db0 db1 ps ys) params =
+    PQ $ \ (Connection conn) -> do
+      let
+        params' =
+          [Just (param, LibPQ.Binary) | param <- toValues (Proxy @ps) params]
+      result <- liftBase $
+        LibPQ.execPrepared conn q params' LibPQ.Binary
+      return (Result <$> result, Connection conn)
+
+instance Monad m => Applicative (PQ db db m) where
+  pure x = PQ $ \ conn -> pure (x, conn)
+  (<*>) = pqAp
+
+instance Monad m => Monad (PQ db db m) where
+  return = pure
+  (>>=) = flip pqBind
+
+instance MonadTrans (PQ db db) where
+  lift m = PQ $ \ conn -> do
     x <- m
     return (x, conn)
 
-instance MonadBase b m => MonadBase b (PQ m db db) where
-  liftBase = ilift . liftBase
+instance MonadBase b m => MonadBase b (PQ db db m) where
+  liftBase = lift . liftBase
 
-type PQRun db = forall m x. Monad m => PQ m db db x -> m (x, Connection db)
+type PQRun db = forall m x. Monad m => PQ db db m x -> m (x, Connection db)
 
-pqliftWith :: Functor m => (PQRun db -> m a) -> PQ m db db a
+pqliftWith :: Functor m => (PQRun db -> m a) -> PQ db db m a
 pqliftWith f = PQ $ \ conn ->
   fmap (\ x -> (x, conn)) (f $ \ pq -> runPQ pq conn)
 
-instance MonadBaseControl b m => MonadBaseControl b (PQ m db db) where
-  type StM (PQ m db db) x = StM m (x, Connection db)
+instance MonadBaseControl b m => MonadBaseControl b (PQ db db m) where
+  type StM (PQ db db m) x = StM m (x, Connection db)
   liftBaseWith f =
     pqliftWith $ \ run -> liftBaseWith $ \ runInBase -> f $ runInBase . run
   restoreM = PQ . const . restoreM
@@ -106,55 +163,6 @@ withConnection connString action =
   bracket (connectdb connString) finish action
 
 newtype Result xs = Result { unResult :: LibPQ.Result }
-
-exec
-  :: MonadBase IO io
-  => Query db0 db1 '[] xs
-  -> PQ io db0 db1 (Maybe (Result xs))
-exec (Query q) = PQ $ \ (Connection conn) -> do
-  result <- liftBase $ LibPQ.exec conn q
-  return (Result <$> result, Connection conn)
-
-execParams
-  :: (MonadBase IO io, ToValues xs ps, ToOids ps)
-  => Query db0 db1 ps ys
-  -> Rec Identity xs
-  -> PQ io db0 db1 (Maybe (Result ys))
-execParams (Query q :: Query db0 db1 ps ys) params =
-  PQ $ \ (Connection conn) -> do
-    let
-      params' =
-        [ Just (oid, param, LibPQ.Binary)
-        | (oid, param) <-
-            zip (toOids (Proxy @ps)) (toValues (Proxy @ps) params)
-        ]
-    result <- liftBase $ LibPQ.execParams conn q params' LibPQ.Binary
-    return (Result <$> result, Connection conn)
-
-prepare
-  :: (MonadBase IO io, ToOids ps)
-  => ByteString
-  -> Query db0 db1 ps xs
-  -> PQ io db0 db0 (Maybe (Result []), PreparedQuery db0 db1 ps xs)
-prepare statementName (Query q :: Query db0 db1 ps ys) =
-  PQ $ \ (Connection conn) -> do
-    result <- liftBase $
-      LibPQ.prepare conn statementName q (Just (toOids (Proxy @ps)))
-    return ((Result <$> result,PreparedQuery statementName), Connection conn)
-
-execPrepared
-  :: (MonadBase IO io, ToValues xs ps)
-  => PreparedQuery db0 db1 ps ys
-  -> Rec Identity xs
-  -> PQ io db0 db1 (Maybe (Result ys))
-execPrepared (PreparedQuery q :: PreparedQuery db0 db1 ps ys) params =
-  PQ $ \ (Connection conn) -> do
-    let
-      params' =
-        [Just (param, LibPQ.Binary) | param <- toValues (Proxy @ps) params]
-    result <- liftBase $
-      LibPQ.execPrepared conn q params' LibPQ.Binary
-    return (Result <$> result, Connection conn)
 
 newtype RowNumber = RowNumber { unRowNumber :: LibPQ.Row }
 
