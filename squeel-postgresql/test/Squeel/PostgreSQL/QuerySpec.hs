80154d97f6e2e94baa24c80579336af6bb993e94
Eitan Chatav <eitan.chatav@gmail.com>
2017-05-01 09:54:07 -0700

    change argument order
    * change the argument order for `where_`, `limit` and `offset` so that
    they are more explicitly monoid/semigroup morphisms:
    
    where_ : (PGBool, &&*, true) -> End Relation
    
    limit : (PGInt8, min) -> End Relation
    
    offset : (PGInt8, +, 0) -> End Relation
    
    * use reverse function application `(&)` instead of infix application
    to replicate sql-like api

--- squeel-postgresql/test/Squeel/PostgreSQL/QuerySpec.hs
+++ squeel-postgresql/test/Squeel/PostgreSQL/QuerySpec.hs
@@ -8,6 +8,7 @@
 module Squeel.PostgreSQL.QuerySpec where
 
 import Data.Boolean
+import Data.Function
 import Data.Proxy
 import Data.Vinyl
 import Test.Hspec
@@ -18,11 +19,11 @@ import Squeel.PostgreSQL.Type
 spec :: Spec
 spec = do
   it "correctly renders a simple SELECT query" $ do
-    renderQuery (select (sumAndCol1 `from` (table1 `where_` true)))
+    renderQuery (select (sumAndCol1 `from` (table1 & where_ true)))
     `shouldBe`
     "SELECT col1+col2 AS sum, col1 AS col1 FROM table1 WHERE TRUE;"
   it "combines WHEREs using AND" $ do
-    renderQuery (select (sumAndCol1 `from` (table1 `where_` true `where_` false)))
+    renderQuery (select (sumAndCol1 `from` (table1 & where_ true & where_ false)))
     `shouldBe`
     "SELECT col1+col2 AS sum, col1 AS col1 FROM table1 WHERE TRUE AND FALSE;"
   it "performs subSELECTs" $ do
@@ -30,23 +31,23 @@ spec = do
     `shouldBe`
     "SELECT * FROM SELECT col1+col2 AS sum, col1 AS col1 FROM table1;"
   it "does LIMIT clauses" $ do
-    renderQuery (select (star `from` (table1 `limit` 1)))
+    renderQuery (select (star `from` (table1 & limit 1)))
     `shouldBe`
     "SELECT * FROM table1 LIMIT 1;"
   it "should use the minimum of given LIMITs" $ do
-    renderQuery (select (star `from` (table1 `limit` 1 `limit` 2)))
+    renderQuery (select (star `from` (table1 & limit 1 & limit 2)))
     `shouldBe`
     "SELECT * FROM table1 LIMIT CASE WHEN 1<=2 THEN 1 ELSE 2 END;"
   it "should render parameters using $ signs" $ do
-    renderQuery (select (star `from` (parameterizedTable1 `limit` param1)))
+    renderQuery (select (star `from` (parameterizedTable1 & limit param1)))
     `shouldBe`
     "SELECT * FROM table1 LIMIT $1;"
   it "does OFFSET clauses" $ do
-    renderQuery (select (star `from` (table1 `offset` 1)))
+    renderQuery (select (star `from` (table1 & offset 1)))
     `shouldBe`
     "SELECT * FROM table1 OFFSET 1;"
   it "should use the sum of given OFFSETs" $ do
-    renderQuery (select (star `from` (table1 `offset` 1 `offset` 2)))
+    renderQuery (select (star `from` (table1 & offset 1 & offset 2)))
     `shouldBe`
     "SELECT * FROM table1 OFFSET 1+2;"
   it "correctly render simple INSERTs" $ do
