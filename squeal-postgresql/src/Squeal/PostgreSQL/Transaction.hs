621a3e8d99e248bfb52741e2699d7b8df015af78
Eitan Chatav <eitan.chatav@gmail.com>
2018-03-01 23:05:01 -0800

    transactions

---
+++ squeal-postgresql/src/Squeal/PostgreSQL/Transaction.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE
+    DataKinds
+  , EmptyCase
+  , FlexibleContexts
+  , FlexibleInstances
+  , LambdaCase
+  , OverloadedStrings
+  , MultiParamTypeClasses
+#-}
+
+module Squeal.PostgreSQL.Transaction
+  ( transactionally
+  , transactionally_
+  , begin
+  , commit
+  , rollback
+  , TransactionMode (..)
+  ) where
+
+import Control.Exception.Lifted
+import Control.Monad.Trans.Control
+import Data.ByteString
+import Data.Monoid
+
+import Squeal.PostgreSQL.Manipulation
+import Squeal.PostgreSQL.Prettyprint
+import Squeal.PostgreSQL.PQ
+
+transactionally
+  :: (MonadBaseControl IO tx, MonadPQ schema tx)
+  => TransactionMode
+  -> tx x -> tx x
+transactionally mode tx = mask $ \restore -> do
+  _ <- begin mode
+  result <- restore tx `onException` rollback
+  _ <- commit
+  return result
+
+transactionally_
+  :: (MonadBaseControl IO tx, MonadPQ schema tx)
+  => tx x -> tx x
+transactionally_ = transactionally (TransactionMode ReadCommitted ReadWrite)
+
+begin :: MonadPQ schema tx => TransactionMode -> tx (Result '[])
+begin mode = manipulate . UnsafeManipulation $
+  "BEGIN" <+> renderTransactionMode mode <> ";"
+
+commit :: MonadPQ schema tx => tx (Result '[])
+commit = manipulate $ UnsafeManipulation "COMMIT;"
+
+rollback :: MonadPQ schema tx => tx (Result '[])
+rollback = manipulate $ UnsafeManipulation "ROLLBACK;"
+
+data TransactionMode = TransactionMode
+  { isolationLevel :: IsolationLevel
+  , readWriteMode  :: ReadWriteMode
+  } deriving (Show, Eq)
+
+renderTransactionMode :: TransactionMode -> ByteString
+renderTransactionMode mode =
+  "ISOLATION LEVEL"
+    <+> renderIsolationLevel (isolationLevel mode)
+    <+> renderReadWriteMode (readWriteMode mode)
+
+data IsolationLevel
+  = Serializable
+  | RepeatableRead
+  | ReadCommitted
+  deriving (Show, Eq)
+
+renderIsolationLevel :: IsolationLevel -> ByteString
+renderIsolationLevel = \case
+  Serializable -> "SERIALIZABLE"
+  ReadCommitted -> "READ COMMITTED"
+  RepeatableRead -> "REPEATABLE READ"
+
+data ReadWriteMode
+  = ReadWrite
+  | ReadOnly
+  deriving (Show, Eq)
+
+renderReadWriteMode :: ReadWriteMode -> ByteString
+renderReadWriteMode = \case
+  ReadWrite -> "READ WRITE"
+  ReadOnly -> "READ ONLY"
