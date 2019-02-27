33ff5bf02d1b834b19f91145b90596aee11da146
Eitan Chatav <eitan@Eitans-MacBook-Pro.local>
2019-02-27 11:23:56 -0800

    cardinality

--- squeal-postgresql/src/Squeal/PostgreSQL/Expression.hs
+++ squeal-postgresql/src/Squeal/PostgreSQL/Expression.hs
@@ -47,6 +47,7 @@ module Squeal.PostgreSQL.Expression
   , nullIf
     -- ** Collections
   , array
+  , cardinality
   , index
   , row
   , field
@@ -482,6 +483,11 @@ array
   -> Expression outer grp commons schemas params from (nullity ('PGvararray ty))
 array xs = UnsafeExpression $
   "ARRAY[" <> commaSeparated (renderSQL <$> xs) <> "]"
+
+cardinality
+  :: Expression outer grp commons schemas params from (nullity ('PGvararray ty))
+  -> Expression outer grp commons schemas params from (nullity 'PGint8)
+cardinality = unsafeFunction "cardinality"
 
 -- | >>> printSQL $ array [null_, false, true] & index 2
 -- (ARRAY[NULL, FALSE, TRUE])[2]
