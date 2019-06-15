8ed0cb1da1472d5fab5ac3c8508586b1271b4aa1
Eitan Chatav <eitan@Eitans-MacBook-Pro.local>
2019-06-14 21:42:49 -0700

    params
    params and insertParams

--- squeal-postgresql/src/Squeal/PostgreSQL/Expression/Parameter.hs
+++ squeal-postgresql/src/Squeal/PostgreSQL/Expression/Parameter.hs
@@ -14,7 +14,9 @@ Parameters, out-of-line data values
   , FlexibleContexts
   , FlexibleInstances
   , FunctionalDependencies
+  , GADTs
   , KindSignatures
+  , LambdaCase
   , MultiParamTypeClasses
   , OverloadedStrings
   , RankNTypes
@@ -27,12 +29,19 @@ Parameters, out-of-line data values
 module Squeal.PostgreSQL.Expression.Parameter
   ( HasParameter (parameter)
   , param
+  , params
+  , insertParamsInto
+  , insertParamsInto_
   ) where
 
+import Data.ByteString (ByteString)
+import Generics.SOP (NP(..), hmap, SListI)
 import GHC.TypeLits
 
+import Squeal.PostgreSQL.Alias
 import Squeal.PostgreSQL.Expression
 import Squeal.PostgreSQL.Expression.Type
+import Squeal.PostgreSQL.Manipulation
 import Squeal.PostgreSQL.Render
 import Squeal.PostgreSQL.Schema
 
@@ -78,3 +87,78 @@ param
    . (PGTyped schemas ty, HasParameter n params ty)
   => Expression outer commons grp schemas params from ty -- ^ param
 param = parameter @n (pgtype @schemas)
+
+class Insertable
+  (schemas :: SchemasType)
+  (params :: [NullityType])
+  (row :: RowType)
+  (columns :: ColumnsType)
+  | columns -> params
+  , columns -> row where
+  params ::
+    NP (Aliased (Optional (
+      Expression '[] commons 'Ungrouped schemas params '[]
+        ) ) ) columns
+instance Insertable schemas '[] '[] '[] where params = Nil
+instance
+  ( KnownSymbol col
+  , PGTyped schemas ty
+  , SListI columns
+  , Insertable schemas params row columns ) =>
+    Insertable schemas (ty ': params) (col ::: ty ': row)
+      (col ::: 'NoDef :=> ty ': columns) where
+        params =
+          Set (param @1) `as` (Alias @col)
+          :* hmap (overValue paramsPlus1) (params @schemas)
+instance
+  ( KnownSymbol col
+  , PGTyped schemas ty
+  , SListI columns
+  , Insertable schemas params row columns ) =>
+    Insertable schemas params row (col ::: 'Def :=> ty ': columns) where
+      params = Default `as` (Alias @col) :* (params @schemas)
+overValue
+  :: ( forall o c g s f x
+      . Expression o c g s p0 f x -> Expression o c g s p1 f x )
+  -> Aliased (Optional (
+        Expression '[] commons 'Ungrouped schemas p0 '[]
+      ) ) column
+  -> Aliased (Optional (
+        Expression '[] commons 'Ungrouped schemas p1 '[]
+      ) ) column
+overValue f = \case
+  Set expr `As` alias -> Set (f expr) `as` alias
+  Default `As` alias -> Default `as` alias
+
+paramsPlus1
+  :: Expression outer commons grp schemas params from ty
+  -> Expression outer commons grp schemas (param ': params) from ty
+paramsPlus1 = UnsafeExpression . paramsPlus1_ . renderSQL
+  where
+    paramsPlus1_ :: ByteString -> ByteString
+    paramsPlus1_ = undefined -- replace $n with $n+1
+
+insertParamsInto
+  :: ( Has sch schemas schema
+     , Has tab schema ('Table table)
+     , columns ~ TableToColumns table
+     , row0 ~ TableToRow table
+     , SListI columns
+     , SListI returning
+     , Insertable schemas params row columns )
+  => QualifiedAlias sch tab
+  -> ConflictClause tab commons schemas params table
+  -> ReturningClause commons schemas params '[tab ::: row0] returning
+  -> Manipulation commons schemas params returning
+insertParamsInto tab = insertInto tab (Values_ params)
+
+insertParamsInto_
+  :: ( Has sch schemas schema
+     , Has tab schema ('Table table)
+     , columns ~ TableToColumns table
+     , row0 ~ TableToRow table
+     , SListI columns
+     , Insertable schemas params row columns )
+  => QualifiedAlias sch tab
+  -> Manipulation commons schemas params '[]
+insertParamsInto_ tab = insertInto_ tab (Values_ params)
