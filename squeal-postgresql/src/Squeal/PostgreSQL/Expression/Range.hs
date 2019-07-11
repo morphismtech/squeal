df5a08abc5f5b379a1ec38549b17237076b94697
Eitan Chatav <eitan@EEM109LMBP-1.local>
2019-07-11 12:59:47 -0700

    rename half interval constructors

--- squeal-postgresql/src/Squeal/PostgreSQL/Expression/Range.hs
+++ squeal-postgresql/src/Squeal/PostgreSQL/Expression/Range.hs
@@ -35,7 +35,7 @@ module Squeal.PostgreSQL.Expression.Range
   ( range
   , Range (..)
   , (<=..<=), (<..<), (<=..<), (<..<=)
-  , halfGT, halfGTE, halfLT, halfLTE
+  , greaterThan, atLeast, lessThan, atMost
   , singleton, whole
   , Bound (..)
   , closed, open
@@ -47,6 +47,7 @@ import qualified GHC.Generics as GHC
 import qualified Generics.SOP as SOP
 
 import Squeal.PostgreSQL.Expression
+import Squeal.PostgreSQL.Expression.Type hiding (bool)
 import Squeal.PostgreSQL.PG
 import Squeal.PostgreSQL.Render
 import Squeal.PostgreSQL.Schema
@@ -54,14 +55,26 @@ import Squeal.PostgreSQL.Schema
 -- $setup
 -- >>> import Squeal.PostgreSQL
 
--- | >>> printSQL $ range (halfGTE now)
--- [now(), )
--- >>> printSQL $ range (0 <..<= (pi & astype numeric))
--- (0, (pi() :: numeric)]
+-- | Construct a `range`
+--
+-- >>> printSQL $ range tstzrange (atLeast now)
+-- tstzrange(now(), NULL, '[)')
+-- >>> printSQL $ range numrange (0 <=..<= 2*pi)
+-- numrange(0, (2 * pi()), '(]')
 range
-  :: Range (Expression outer commons grp schemas params from ('NotNull ty))
+  :: TypeExpression schemas (null ('PGrange ty))
+  -> Range (Expression outer commons grp schemas params from ('NotNull ty))
   -> Expression outer commons grp schemas params from (null ('PGrange ty))
-range = UnsafeExpression . renderSQL
+range ty = \case
+  Empty -> UnsafeExpression "empty"
+  NonEmpty l u -> UnsafeExpression $ renderSQL ty <> parenthesized
+    (commaSeparated [renderArg l, renderArg u, renderClopen l u])
+    where
+      renderArg (Bound _ x) = maybe "NULL" renderSQL x
+      clopenLeft = bool "(" "["
+      clopenRight = bool ")" "]"
+      renderClopen (Bound l' _) (Bound u' _) =
+        "\'" <> clopenLeft l' <> clopenRight u' <> "\'"
 
 data Bound x = Bound
   { closedBound :: Bool
@@ -83,27 +96,18 @@ data Range x
   deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
 type instance PG (Range x) = 'PGrange (PG x)
 
-instance RenderSQL x => RenderSQL (Range x) where
-  renderSQL = \case
-    Empty -> "empty"
-    NonEmpty l u -> commaSeparated [renderLower l, renderUpper u]
-      where
-        renderLower (Bound isclosed x) =
-          bool "(" "[" isclosed <> maybe "" renderSQL x
-        renderUpper (Bound isclosed x) =
-          maybe "" renderSQL x <> bool ")" "]" isclosed
-
 (<=..<=), (<..<), (<=..<), (<..<=) :: x -> x -> Range x
+infix 4 <=..<=, <..<, <=..<, <..<=
 x <=..<= y = NonEmpty (closed (Just x)) (closed (Just y))
 x <..< y = NonEmpty (open (Just x)) (open (Just y))
 x <=..< y = NonEmpty (closed (Just x)) (open (Just y))
 x <..<= y = NonEmpty (open (Just x)) (closed (Just y))
 
-halfGT, halfGTE, halfLT, halfLTE :: x -> Range x
-halfGT x = NonEmpty (open (Just x)) (open Nothing)
-halfGTE x = NonEmpty (closed (Just x)) (open Nothing)
-halfLT x = NonEmpty (open Nothing) (open (Just x))
-halfLTE x = NonEmpty (open Nothing) (closed (Just x))
+greaterThan, atLeast, lessThan, atMost :: x -> Range x
+greaterThan x = NonEmpty (open (Just x)) (open Nothing)
+atLeast x = NonEmpty (closed (Just x)) (open Nothing)
+lessThan x = NonEmpty (open Nothing) (open (Just x))
+atMost x = NonEmpty (open Nothing) (closed (Just x))
 
 singleton :: x -> Range x
 singleton x = x <=..<= x
