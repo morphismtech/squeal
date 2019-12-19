bc79aac236f1fc2e88d8556e36fdd0ba6e0458b5
Eitan Chatav <eitan@EEM109LMBP-1.local>
2019-12-19 12:50:18 -0800

    Update Parameter.hs

--- squeal-postgresql/src/Squeal/PostgreSQL/Expression/Parameter.hs
+++ squeal-postgresql/src/Squeal/PostgreSQL/Expression/Parameter.hs
@@ -76,6 +76,6 @@ instance {-# OVERLAPPABLE #-} (KnownNat n, HasParameter (n-1) params ty)
 -- ($1 :: int4)
 param
   :: forall n ty outer commons db params from grp
-   . (NullityTyped db ty, HasParameter n params ty)
+   . (NullTyped db ty, HasParameter n params ty)
   => Expression outer commons grp db params from ty -- ^ param
-param = parameter @n (nullitytype @db)
+param = parameter @n (nulltype @db)
