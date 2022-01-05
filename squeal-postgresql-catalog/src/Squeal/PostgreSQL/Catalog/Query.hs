8122cdd2616b19f203a45e05fecd1c6ef7e829ba
Eitan Chatav <eitan.chatav@gmail.com>
2022-01-05 13:34:35 -0800

    selectComposites

--- squeal-postgresql-catalog/src/Squeal/PostgreSQL/Catalog/Query.hs
+++ squeal-postgresql-catalog/src/Squeal/PostgreSQL/Catalog/Query.hs
@@ -9,7 +9,7 @@ module Squeal.PostgreSQL.Catalog.Query where
 import Squeal.PostgreSQL
 import Squeal.PostgreSQL.Catalog
 
-selectEnums :: Query '[] '[] DB params '[
+selectEnums :: Query lat with DB params '[
   "nspname" ::: 'NotNull 'PGtext,
   "typname" ::: 'NotNull 'PGtext,
   "enumlabels" ::: 'NotNull ('PGvararray ('NotNull 'PGtext))]
@@ -32,3 +32,42 @@ selectEnums = select_
     )
     & groupBy (#n ! #nspname :* #t ! #typname)
   )
+
+selectComposites :: Query lat with DB params '[
+  "nspname" ::: 'NotNull 'PGtext,
+  "typname" ::: 'NotNull 'PGtext,
+  "fields" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
+    "attname" ::: 'NotNull 'PGtext,
+    "nspname" ::: 'NotNull 'PGtext,
+    "typname" ::: 'NotNull 'PGtext])))]
+selectComposites =
+  from
+    ( table (#pg_catalog ! #pg_type `as` #t)
+      & innerJoin (table (#pg_catalog ! #pg_class `as` #c))
+        (#c ! #reltype .== #t ! #oid)
+      & innerJoin (table (#pg_catalog ! #pg_attribute `as` #a))
+        (#a ! #atttypid .== #t ! #oid)
+      & innerJoin (table (#pg_catalog ! #pg_namespace `as` #n))
+        (#n ! #oid .== #t ! #typnamespace)
+      & innerJoin (table (#pg_catalog ! #pg_type `as` #at))
+        (#at ! #oid .== #a ! #atttypid)
+      & innerJoin (table (#pg_catalog ! #pg_namespace `as` #an))
+        (#an ! #oid .== #at ! #typnamespace)
+    )
+  & where_ (#t ! #typtype .== inline 'c')
+  & where_ (#c ! #relkind .== inline 'c')
+  & groupBy (#n ! #nspname :* #t ! #typname)
+  & select_
+    ( mapAliased (cast text) (#n ! #nspname) :*
+      mapAliased (cast text) (#t ! #typname) :*
+      fromNull (array0 record)
+        ( arrayAgg
+          ( All (row (
+              mapAliased (cast text) (#a ! #attname) :*
+              mapAliased (cast text) (#an ! #nspname) *:
+              mapAliased (cast text) (#at ! #typname)
+              ))
+            & orderBy [Asc (#a ! #attnum)]
+          )
+        ) `as` #fields
+    )
