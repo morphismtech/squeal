df10c5e0078c821256d456febd66d0af27da7d48
Eitan Chatav <eitan.chatav@gmail.com>
2022-01-26 11:58:34 -0800

    third pass minimization

--- squeal-postgresql/src/Squeal/PostgreSQL/Type/Schema.hs
+++ squeal-postgresql/src/Squeal/PostgreSQL/Type/Schema.hs
@@ -1,38 +1,3 @@
-{-|
-Module: Squeal.PostgreSQL.Type.Schema
-Description: Postgres type system
-Copyright: (c) Eitan Chatav, 2019
-Maintainer: eitan@morphism.tech
-Stability: experimental
-
-Provides a type-level DSL for kinds of Postgres types,
-tables, schema, constraints, and more.
-It also defines useful type families to operate on these.
--}
-
-{-# LANGUAGE
-    AllowAmbiguousTypes
-  , ConstraintKinds
-  , DeriveAnyClass
-  , DeriveGeneric
-  , FlexibleContexts
-  , FlexibleInstances
-  , FunctionalDependencies
-  , GADTs
-  , LambdaCase
-  , OverloadedStrings
-  , QuantifiedConstraints
-  , RankNTypes
-  , ScopedTypeVariables
-  , StandaloneDeriving
-  , TypeApplications
-  , TypeFamilyDependencies
-  , TypeInType
-  , TypeOperators
-  , UndecidableInstances
-  , UndecidableSuperClasses
-#-}
-
 module Squeal.PostgreSQL.Type.Schema
   ( -- * Postgres Type
     PGType (..)
