b1ac5d4a701554a58ac5ce8ad38b3ba6c17c76c4
Eitan Chatav <eitan.chatav@gmail.com>
2024-08-30 18:42:00 -0700

    chugging along

--- squeal-postgresql-catalog/src/Squeal/PostgreSQL/Catalog/Query.hs
+++ squeal-postgresql-catalog/src/Squeal/PostgreSQL/Catalog/Query.hs
@@ -14,10 +14,8 @@
 
 module Squeal.PostgreSQL.Catalog.Query where
 
-import Data.Foldable
 import qualified Generics.SOP as SOP
 import qualified GHC.Generics as GHC
-import Language.Haskell.TH.Syntax hiding (Inline)
 import Squeal.PostgreSQL
 import Squeal.PostgreSQL.Catalog
 
@@ -32,33 +30,83 @@ deriving via VarArray [Composite ObjSchemum]
 deriving via VarArray [Composite ObjSchemum]
   instance Inline [ObjSchemum]
 
-getEnums :: [ObjSchemum] -> Statement DB () (ObjSchemum, Type)
-getEnums objs =
+data ObjType = ObjType
+  { typobj :: ObjSchemum
+  , typtype :: Char
+  , typarray :: ObjArrayType
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjType
+
+data ObjArrayType = ObjArrayType
+  { arrtypnspname :: Maybe String
+  , arrtypname :: Maybe String
+  , arrtyptype :: Maybe Char
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjArrayType
+
+data ObjAtt = ObjAtt
+  { attname :: String
+  , atthasdef :: Bool
+  , typ :: ObjType
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjAtt
+
+data ObjEnum = ObjEnum
+  { enumobj :: ObjSchemum
+  , enumlabels :: VarArray [String]
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjEnum
+
+data ObjCols = ObjCols
+  { colstbl :: ObjSchemum
+  , attributes :: VarArray [ObjAtt]
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjCols
+
+data ObjCon = ObjCon
+  { conname :: String
+  , contype :: Char
+  , conkey_columns :: VarArray [String]
+  , confrel_schema :: Maybe String
+  , confrel_name :: Maybe String
+  , confkey_columns :: Maybe (VarArray [String])
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjCon
+deriving via VarArray [Composite ObjCon]
+  instance IsPG [ObjCon]
+deriving via VarArray [Composite ObjCon]
+  instance Inline [ObjCon]
+deriving via VarArray [Composite ObjCon]
+  instance FromPG [ObjCon]
+
+data ObjCons = ObjCons
+  { constbl :: ObjSchemum
+  , constraints :: [ObjCon]
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite ObjCons
+
+getEnums :: Maybe [ObjSchemum] -> Statement DB () ObjEnum
+getEnums objsMaybe =
   let
-    cond = case objs of
-      [] -> true
-      _ -> arrAny #obj (.==) (inline objs)
+    cond = case objsMaybe of
+      Nothing -> true
+      Just objs -> arrAny #enumobj (.==) (inline objs)
     sql
       = from (subquery (selectEnums `as` #enums))
       & where_ cond
       & select Star
-    enc = nilParams
-    dec = do
-      obj <- #obj
-      VarArray enumlabels <- #enumlabels
-      let
-        consLabel ty str = AppT
-          (AppT PromotedConsT (LitT (StrTyLit str))) ty
-        labels = foldl' consLabel PromotedNilT (enumlabels :: [String])
-        enumType = AppT
-          (PromotedT (mkName "Typedef"))
-          (AppT (PromotedT (mkName "PGenum")) labels)
-      return (obj, enumType)
   in
-    Query enc dec sql
+    query sql
 
 selectEnums :: Query lat with DB params '[
-  "obj" ::: NullPG ObjSchemum,
+  "enumobj" ::: NullPG ObjSchemum,
   "enumlabels" ::: 'NotNull ('PGvararray ('NotNull 'PGtext))]
 selectEnums =
   from
@@ -72,12 +120,30 @@ selectEnums =
     ( row
         ( mapAliased (cast text) (#nsp ! #nspname) :*
           cast text (#typ ! #typname) `as` #objname
-        ) `as` #obj :*
+        ) `as` #enumobj :*
       fromNull (array0 text)
         ( arrayAgg
           ( All (cast text (#enum ! #enumlabel))
             & orderBy [Asc (#enum ! #enumsortorder)]
           ) ) `as` #enumlabels )
+
+getColumns
+  :: Maybe [ObjSchemum]
+  -> Statement DB () ObjCols
+getColumns objsMaybe =
+  let
+    cond = case objsMaybe of
+      Nothing -> true
+      Just objs -> arrAny #obj (.==) (inline objs)
+    sql
+      = from (subquery (selectRelations `as` #rels))
+      & where_ (#relkind .== inline 'r')
+      & where_ cond
+      & select_
+        ( #obj `as` #colstbl :*
+          #attributes )
+  in
+    query sql
 
 selectRelations :: Query lat with DB params '[
   "obj" ::: 'NotNull (PG ObjSchemum),
@@ -85,8 +151,7 @@ selectRelations :: Query lat with DB params '[
   "attributes" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
     "attname" ::: 'NotNull 'PGtext,
     "atthasdef" ::: 'NotNull 'PGbool,
-    "typname" ::: 'NotNull 'PGtext,
-    "typtype" ::: 'NotNull ('PGchar 1)])))]
+    "typ" ::: NullPG ObjType ])))]
 selectRelations =
   from
     ( table (#pg_catalog ! #pg_namespace `as` #nsp)
@@ -94,10 +159,14 @@ selectRelations =
         (#rel ! #relnamespace .== #nsp ! #oid)
       & innerJoin (table (#pg_catalog ! #pg_attribute `as` #att))
         (#att ! #attrelid .== #rel ! #oid)
-      & innerJoin (table (#pg_catalog ! #pg_type `as` #atttyp))
-        (#atttyp ! #oid .== #att ! #atttypid)
-      & innerJoin (table (#pg_catalog ! #pg_namespace `as` #attnsp))
-        (#attnsp ! #oid .== #atttyp ! #typnamespace) )
+      & innerJoin (table (#pg_catalog ! #pg_type `as` #typ))
+        (#typ ! #oid .== #att ! #atttypid)
+      & innerJoin (table (#pg_catalog ! #pg_namespace `as` #typnsp))
+        (#typnsp ! #oid .== #typ ! #typnamespace)
+      & leftOuterJoin (table (#pg_catalog ! #pg_type `as` #arrtyp))
+        (#arrtyp ! #oid .== #typ ! #typarray)
+      & leftOuterJoin (table (#pg_catalog ! #pg_namespace `as` #arrnsp))
+        (#arrnsp ! #oid .== #arrtyp ! #typnamespace) )
   & groupBy (#nsp ! #nspname :* #rel ! #relname :* #rel ! #relkind)
   & select_
     ( row
@@ -110,14 +179,48 @@ selectRelations =
           ( All (row (
               mapAliased (cast text) (#att ! #attname) :*
               #att ! #atthasdef :*
-              mapAliased (cast text) (#atttyp ! #typname) :*
-              #atttyp ! #typtype ))
+              row
+                ( row
+                    ( mapAliased (cast text) (#typnsp ! #nspname) :*
+                      cast text (#typ ! #typname) `as` #objname
+                    ) `as` #typobj :*
+                  #typ ! #typtype `as` #typtype :*
+                  row
+                    ( cast text (#arrnsp ! #nspname) `as` #arrtypnspname :*
+                      cast text (#arrtyp ! #typname) `as` #arrtypname :*
+                      #arrtyp ! #typtype `as` #arrtyptype
+                    ) `as` #typarray
+                ) `as` #typ
+              ) )
             & orderBy [Asc (#att ! #attnum)]
           ) ) `as` #attributes )
 
+getTableConstraints
+  :: Maybe [ObjSchemum]
+  -> Statement DB () ObjCons
+getTableConstraints objsMaybe =
+  let
+    cond = case objsMaybe of
+      Nothing -> true
+      Just objs -> arrAny #tbl (.==) (inline objs)
+    sql
+      = from (subquery (selectTableConstraints `as` #cons))
+      & where_ cond
+      & groupBy #tbl
+      & select_
+        ( #tbl `as` #constbl :*
+          fromNull (array0 record) (arrayAgg (All (row (
+            #conname :*
+            #contype :*
+            #conkey_columns :*
+            #confrel_schema :*
+            #confrel_name :*
+            #confkey_columns )))) `as` #constraints )
+  in
+    query sql
+
 selectTableConstraints :: Query lat with DB params '[
-  "nspname" ::: 'NotNull 'PGtext,
-  "relname" ::: 'NotNull 'PGtext,
+  "tbl" ::: NullPG ObjSchemum,
   "conname" ::: 'NotNull 'PGtext,
   "contype" ::: 'NotNull ('PGchar 1),
   "conkey_columns" ::: 'NotNull ('PGvararray ('NotNull 'PGtext)),
@@ -149,14 +252,15 @@ selectTableConstraints =
        #confrel_nsp ! #nspname :*
        #confrel ! #relname )
   & select_
-    ( mapAliased (cast text) (#nsp ! #nspname) :*
-      mapAliased (cast text) (#rel ! #relname) :*
+    ( row
+        ( mapAliased (cast text) (#nsp ! #nspname) :*
+          cast text (#rel ! #relname) `as` #objname
+        ) `as` #tbl :*
       mapAliased (cast text) (#con ! #conname) :*
       #con ! #contype :*
       fromNull (array0 text)
         (arrayAgg (All (cast text (#att ! #attname))))
         `as` #conkey_columns :*
-
       cast text (#confrel_nsp ! #nspname) `as` #confrel_schema :*
       cast text (#confrel ! #relname) `as` #confrel_name :*
       arrayAgg (All (cast text (#confatt ! #attname)))
