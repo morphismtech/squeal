60cf41d7c5b5b71fd167c1fc346045ea4884418b
Eitan Chatav <eitan.chatav@gmail.com>
2024-09-11 07:57:50 -0700

    Update Query2.hs

--- squeal-postgresql-catalog/src/Squeal/PostgreSQL/Catalog/Query2.hs
+++ squeal-postgresql-catalog/src/Squeal/PostgreSQL/Catalog/Query2.hs
@@ -12,6 +12,7 @@
   , PolyKinds
   , RankNTypes
   , RecordWildCards
+  , ScopedTypeVariables
   , StandaloneDeriving
   , TypeOperators
   , UndecidableInstances
@@ -21,19 +22,66 @@ module Squeal.PostgreSQL.Catalog.Query2 where
 
 import Control.Monad
 import Data.Foldable
+import Data.Traversable
 import Data.Int
 import qualified Generics.SOP as SOP
 import qualified GHC.Generics as GHC
-import Language.Haskell.TH.Syntax hiding (Inline)
+import Language.Haskell.Exts.Syntax
 import Squeal.PostgreSQL
 import Squeal.PostgreSQL.Catalog
 
-type ObjSchemum = 'PGcomposite '[
-  "nspname" ::: 'NotNull 'PGtext,
-  "objname" ::: 'NotNull 'PGtext]
+data SchObj = SchObj {nspname :: String, objname :: String}
+  deriving stock GHC.Generic
+  deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+  deriving (IsPG, FromPG) via Composite SchObj
+
+formatObj :: SchObj -> String
+formatObj (SchObj "public" obj) = obj
+formatObj (SchObj sch obj) = sch <> "_" <> obj
+
+data SchCon = SchCon
+  { conname :: String
+  , contype :: Char
+  , conkey_columns :: VarArray [String]
+  , confrel_schema :: Maybe String
+  , confrel_name :: Maybe String
+  , confkey_columns :: Maybe (VarArray [String])
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, Inline, FromPG) via Composite SchCon
+deriving via VarArray [Composite SchCon]
+  instance IsPG [SchCon]
+deriving via VarArray [Composite SchCon]
+  instance Inline [SchCon]
+deriving via VarArray [Composite SchCon]
+  instance FromPG [SchCon]
+
+data SchType = SchType
+  { typobj :: SchObj
+  , typtype :: Char
+  , typcategory :: Char
+  , typlen :: Int16
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, FromPG) via Composite SchType
+
+data SchAtt = SchAtt
+  { attname :: String
+  , atthasdef :: Bool
+  , attnotnull :: Bool
+  , typ :: SchType
+  } deriving stock GHC.Generic
+    deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+    deriving (IsPG, FromPG) via Composite SchAtt
+deriving via VarArray [Composite SchAtt]
+  instance IsPG [SchAtt]
+deriving via VarArray [Composite SchAtt]
+  instance FromPG [SchAtt]
 
 selectEnums :: Query lat with DB params '[
-  "enumobj" ::: 'NotNull ObjSchemum,
+  "enumobj" ::: 'NotNull ('PGcomposite '[
+    "nspname" ::: 'NotNull 'PGtext,
+    "objname" ::: 'NotNull 'PGtext]),
   "enumlabels" ::: 'NotNull ('PGvararray ('NotNull 'PGtext))]
 selectEnums =
   from
@@ -53,9 +101,27 @@ selectEnums =
           ( All (cast text (#enum ! #enumlabel))
             & orderBy [Asc (#enum ! #enumsortorder)]
           ) ) `as` #enumlabels )
+decodeEnum :: DecodeRow '[
+  "enumobj" ::: 'NotNull ('PGcomposite '[
+    "nspname" ::: 'NotNull 'PGtext,
+    "objname" ::: 'NotNull 'PGtext]),
+  "enumlabels" ::: 'NotNull ('PGvararray ('NotNull 'PGtext))]
+  (SchObj, Type ())
+decodeEnum = do
+  obj <- #enumobj
+  VarArray lbls <- #enumlabels
+  let
+    tySymbol s = TyPromoted () (PromotedString () s s)
+    tyList = TyPromoted () . PromotedList () False
+    tyDataKind s = TyCon () (UnQual () (Ident () s))
+    pgEnum = TyApp () (tyDataKind "PGenum")
+    enumLbls = tyList [tySymbol lbl | lbl <- lbls]
+  return (obj, pgEnum enumLbls)
 
 selectTables :: Query '[] with DB params '[
-  "tblobj" ::: 'NotNull ObjSchemum,
+  "tblobj" ::: 'NotNull ('PGcomposite '[
+    "nspname" ::: 'NotNull 'PGtext,
+    "objname" ::: 'NotNull 'PGtext]),
   "tblcons" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
     "conname" ::: 'NotNull 'PGtext,
     "contype" ::: 'NotNull ('PGchar 1),
@@ -66,8 +132,11 @@ selectTables :: Query '[] with DB params '[
   "tblcols" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
     "attname" ::: 'NotNull PGtext,
     "atthasdef" ::: 'NotNull PGbool,
+    "attnotnull" ::: 'NotNull PGbool,
      "typ" ::: 'NotNull ('PGcomposite '[
-      "typobj" ::: 'NotNull ObjSchemum,
+      "typobj" ::: 'NotNull ('PGcomposite '[
+        "nspname" ::: 'NotNull 'PGtext,
+        "objname" ::: 'NotNull 'PGtext]),
       "typtype" ::: 'NotNull ('PGchar 1),
       "typcategory" ::: 'NotNull ('PGchar 1),
       "typlen" ::: 'NotNull 'PGint2])])))]
@@ -100,15 +169,63 @@ selectTables =
       ( #cons ! #obj `as` #tblobj :*
         #cons ! #constraints `as` #tblcons :*
         #cols ! #attributes `as` #tblcols )
+decodeTable :: DecodeRow '[
+  "tblobj" ::: 'NotNull ('PGcomposite '[
+    "nspname" ::: 'NotNull 'PGtext,
+    "objname" ::: 'NotNull 'PGtext]),
+  "tblcons" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
+    "conname" ::: 'NotNull 'PGtext,
+    "contype" ::: 'NotNull ('PGchar 1),
+    "conkey_columns" ::: 'NotNull ('PGvararray ('NotNull 'PGtext)),
+    "confrel_schema" ::: 'Null 'PGtext,
+    "confrel_name" ::: 'Null 'PGtext,
+    "confkey_columns" ::: 'Null ('PGvararray ('NotNull 'PGtext))]))),
+  "tblcols" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
+    "attname" ::: 'NotNull PGtext,
+    "atthasdef" ::: 'NotNull PGbool,
+    "attnotnull" ::: 'NotNull PGbool,
+     "typ" ::: 'NotNull ('PGcomposite '[
+      "typobj" ::: 'NotNull ('PGcomposite '[
+        "nspname" ::: 'NotNull 'PGtext,
+        "objname" ::: 'NotNull 'PGtext]),
+      "typtype" ::: 'NotNull ('PGchar 1),
+      "typcategory" ::: 'NotNull ('PGchar 1),
+      "typlen" ::: 'NotNull 'PGint2])])))]
+  (SchObj, Type (), Type ())
+decodeTable = do
+  undefined
+  -- tbl <- #tblobj
+  -- VarArray (cons :: [SchCon]) <- #tblcons
+  -- VarArray (cols :: [SchAtt]) <- #tblcols
+  -- let
+  --   tySymbol s = TyPromoted () (PromotedString () s s)
+  --   tyList = TyPromoted () . PromotedList () False
+  --   tyDataKind s = TyCon () (UnQual () (Ident () s))
+  --   tyInfix x s y = TyInfix () x (UnpromotedName () (UnQual () (Ident () s))) y
+  --   -- pgEnum = TyApp () (tyDataKind "PGenum")
+  --   -- enumLbls = tyList [tySymbol lbl | lbl <- lbls]
+  -- columnTys <- for cols $ \SchAtt{..} -> do
+  --   let
+  --     SchType {..} = typ
+  --     deflt = tyDataKind (if atthasdef then "Def" else "NoDef")
+  --     nullty = TyApp () (tyDataKind (if attnotnull then "NotNull" else "Null"))
+  --     column = tyInfix (tySymbol attname) ":::" (tyInfix deflt ":=>" nullty _) 
+  --   return column
+  -- return (tbl, _, cols)
 
 selectRelations :: Query lat with DB params '[
-  "obj" ::: 'NotNull ObjSchemum,
+  "obj" ::: 'NotNull ('PGcomposite '[
+    "nspname" ::: 'NotNull 'PGtext,
+    "objname" ::: 'NotNull 'PGtext]),
   "relkind" ::: 'NotNull ('PGchar 1),
   "attributes" ::: 'NotNull ('PGvararray ('NotNull ('PGcomposite '[
     "attname" ::: 'NotNull 'PGtext,
     "atthasdef" ::: 'NotNull 'PGbool,
+    "attnotnull" ::: 'NotNull 'PGbool,
     "typ" ::: 'NotNull ('PGcomposite '[
-      "typobj" ::: 'NotNull ObjSchemum,
+      "typobj" ::: 'NotNull ('PGcomposite '[
+        "nspname" ::: 'NotNull 'PGtext,
+        "objname" ::: 'NotNull 'PGtext]),
       "typtype" ::: 'NotNull ('PGchar 1),
       "typcategory" ::: 'NotNull ('PGchar 1),
       "typlen" ::: 'NotNull 'PGint2])])))]
@@ -138,6 +255,7 @@ selectRelations =
           ( All (row (
               mapAliased (cast text) (#att ! #attname) :*
               #att ! #atthasdef :*
+              #att ! #attnotnull :*
               row
                 ( row
                     ( mapAliased (cast text) (#typnsp ! #nspname) :*
@@ -152,7 +270,9 @@ selectRelations =
           ) ) `as` #attributes )
 
 selectTableConstraints :: Query lat with DB params '[
-  "obj" ::: 'NotNull ObjSchemum,
+  "obj" ::: 'NotNull ('PGcomposite '[
+    "nspname" ::: 'NotNull 'PGtext,
+    "objname" ::: 'NotNull 'PGtext]),
   "conname" ::: 'NotNull 'PGtext,
   "contype" ::: 'NotNull ('PGchar 1),
   "conkey_columns" ::: 'NotNull ('PGvararray ('NotNull 'PGtext)),
